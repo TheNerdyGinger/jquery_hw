0f3741711e39cf72b9caeb95ef89674f7695c611
TheNerdyGinger <stephenfedewa@hotmail.com>
2019-11-21 09:43:21 -0500

    fixed tabbing

--- js/challenge5.js
+++ js/challenge5.js
@@ -2,23 +2,23 @@ console.log("here")
 var imgs = document.querySelectorAll("img");
 var msg = "Hover over an image below."
 for (let i = 0; i < imgs.length;i++){
-		imgs[i].onmouseover = function() {
-		 	$('#image').css("backgroundImage", "url('"+imgs[i].src+"')");
-		 	$('#image').html(imgs[i].alt);
-	 	}
+	imgs[i].onmouseover = function() {
+		$('#image').css("backgroundImage", "url('"+imgs[i].src+"')");
+		$('#image').html(imgs[i].alt);
+	 }
 
-	 	imgs[i].onfocus = function() {
-			$('#image').css("backgroundImage", "url('"+imgs[i].src+"')");
-		 	$('#image').html(imgs[i].alt);
-	 	}
+	 imgs[i].onfocus = function() {
+		$('#image').css("backgroundImage", "url('"+imgs[i].src+"')");
+		$('#image').html(imgs[i].alt);
+	 }
 
 	 imgs[i].onmouseleave = function() {
-		 	$('#image').css("backgroundImage", "url('')");
-		 	$('#image').html(msg);
+		 $('#image').css("backgroundImage", "url('')");
+		 $('#image').html(msg);
 	 }
 
 	 imgs[i].onblur = function() {
-		 	$('#image').css("backgroundImage", "url('')");
-		 	$('#image').html(msg);
+		 $('#image').css("backgroundImage", "url('')");
+		 $('#image').html(msg);
 	 }
 }
